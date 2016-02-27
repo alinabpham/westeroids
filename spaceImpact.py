08490e2a208d81d49b72ae5b10e683bd11bc33ce
rguarino <ryanguarino@Ryans-MacBook-Pro.local>
2016-02-27 17:39:41 -0500

    droid

--- spaceImpact.py
+++ spaceImpact.py
@@ -4,12 +4,38 @@ pygame.init()
 
 
 size = width,height = 500,500
+black = 0, 0, 0
+
 screen = pygame.display.set_mode(size)
+<<<<<<< Updated upstream
 black = 0,0,0
+
+
 player = pygame.image.load("spaceship copy.png").convert()
-player_rect = player.get_rect()
+=======
+
+player = pygame.image.load("spaceship copy.bmp").convert()
 pygame.transform.scale(player, (10, 10))
+>>>>>>> Stashed changes
+player_rect = player.get_rect()
+<<<<<<< HEAD
+
+
+pygame.transform.scale(player, (10, 10))
+
+
+player = pygame.image.load("galaxy1 copy.bmp")
+pygame.transform.scale(player, (10, 10))
+
+player = pygame.image.load("spaceship copy.bmp")
+
+player_rect = player.get_rect()
+enemy = pygame.image.load("droid.png")
+enemy_rect = enemy.get_rect()
 background = pygame.image.load("galaxy1 copy.bmp").get_rect()
+=======
+background = pygame.image.load("galaxy1 copy.bmp").convert().get_rect()
+>>>>>>> origin/master
 
 
 
@@ -17,13 +43,8 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    #screen.fill(black)
+    screen.fill(black)
     screen.blit(player, player_rect)
+    screen.blit(enemy, enemy_rect)
 
     pygame.display.flip()
-
-
-
-# player = pygame.image.load("spaceship copy.bmp").convert()
-# background = pygame.image.load("galaxy1 copy.bmp").convert()
-# screen.blit(background, (0, 0))

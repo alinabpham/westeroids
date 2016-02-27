e22fe927d5fdd6d688fde4a6e05e9d37b2d85036
Mario See <mariocsee@gmail.com>
2016-02-27 13:37:25 -0500

    tests

--- spaceImpact.py
+++ spaceImpact.py
@@ -1,4 +1,14 @@
-import pygame
+import sys, pygame
+from pygame.locals import *
+
 pygame.init()
 
-size = width, height = 400,400
+size = width, height = 500, 240
+speed = [2, 2]
+BLACK = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+screen.fill(BLACK)
+
+spaceship = pygame.image.load("deathstar.jpg")

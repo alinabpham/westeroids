673c354680d84f70667513d75de6aa5d067e1925
alinabpham <alinabpham@gmail.com>
2016-02-27 17:57:12 -0500

    scroll again

--- spaceImpact.py
+++ spaceImpact.py
@@ -8,27 +8,18 @@ black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-<<<<<<< HEAD
-player = pygame.image.load("spaceship copy.png").convert()
-=======
+
 #load player spaceship
 player = pygame.image.load("spaceship copy.png")
 pygame.transform.scale(player, (10, 10))
->>>>>>> origin/master
 player_rect = player.get_rect()
 
 #Load droid enemy image
 enemy = pygame.image.load("droid.png")
 enemy_rect = enemy.get_rect()
-<<<<<<< HEAD
-=======
 
 #Load background image
 background = pygame.image.load("galaxy1 copy.bmp").convert()
-
->>>>>>> origin/master
-
-background = pygame.image.load("galaxy1 copy.bmp")
 background_rect = background.get_rect()
 
 w,h = background.get_size()
@@ -47,20 +38,18 @@ while 1:
     screen.fill(black)
     #Puts background in screen
     screen.blit(background, [0,0])
-    #Puts player in screen
-    screen.blit(player, player_rect)
-    #Puts enemy in screen
-    screen.blit(enemy, enemy_rect)
-
-    y1 += 5
-    y += 5
+    y1 += 1
+    y += 1
     screen.blit(background,(x,y))
     screen.blit(background,(x1,y1))
     if y > h:
         y = -h
     if y1 > h:
         y1 = -h
-
+    #Puts player in screen
+    screen.blit(player, player_rect)
+    #Puts enemy in screen
+    screen.blit(enemy, enemy_rect)
 
     pygame.display.update()
     pygame.display.flip()

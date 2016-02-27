f2a4f2b1b48ff3807bb431512eb958e8a3bbcb8a
Mario See <mariocsee@gmail.com>
2016-02-27 17:35:15 -0500

    remove transform function
    - remove trasform.scale

--- spaceImpact.py
+++ spaceImpact.py
@@ -8,7 +8,6 @@ screen = pygame.display.set_mode(size)
 black = 0,0,0
 player = pygame.image.load("spaceship copy.png").convert()
 player_rect = player.get_rect()
-pygame.transform.scale(player, (10, 10))
 background = pygame.image.load("galaxy1 copy.bmp").get_rect()
 
 
@@ -17,7 +16,7 @@ while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    #screen.fill(black)
+    screen.fill(black)
     screen.blit(player, player_rect)
 
     pygame.display.flip()
